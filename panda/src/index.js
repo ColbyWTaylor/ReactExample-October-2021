daf01d08cc932e7673aa44528710daa8c0d11c2e
ColbyWTaylor <colbyt.093@gmail.com>
2021-10-06 08:41:21 +0000

    useState examples

--- panda/src/index.js
+++ panda/src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import Panda from './Panda';
 
-const ninjaList = [
-  {id: 1, name: "Raphael"},
-  {id: 1, name: "Leonardo"},
-  {id: 1, name: "Donatello"},
-  {id: 1, name: "Mikey"}
-]
 
 function App({ninjas}) {
+  const [status, setStatus] = useState("Open")
+  const [show,setShow] = useState("White Lotus")
+  const [numberOfViewers, setNumberOfViewers] = useState(0)
   return (
     <>
-    {ninjas.map(ninja => (
-      <li key={ninja.id}>{ninja.name}</li>
-    ))}
+    <p>The best show right now is {show}. It has {numberOfViewers} viewers</p>
+    <button onClick={()=> setShow("Squid Games")}>update show to squid games</button>
+    <button onClick={()=> setNumberOfViewers(numberOfViewers + 1)}>Increase Viewers</button>
+    <p>useState status is {status}</p>
+    <button onClick={()=> setStatus("Closed")}>Click me</button>
+    <button onClick={()=> setStatus("Open")}>Click me</button>
     </>
   )
 }
 
 ReactDOM.render(
-  <App ninjas={ninjaList} />,
+  <App />,
   document.getElementById('root')
 );
