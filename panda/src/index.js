0287d264b4d44b74b9e645f11edb30893d17986d
ColbyWTaylor <colbyt.093@gmail.com>
2021-10-06 08:55:41 +0000

    checkbox example

--- panda/src/index.js
+++ panda/src/index.js
@@ -4,24 +4,16 @@ import './index.css';
 // import App from './App';
 // import Panda from './Panda';
 
-
-function App({ninjas}) {
-  const [status, setStatus] = useState("Open")
-  const [show,setShow] = useState("White Lotus")
-  const [numberOfViewers, setNumberOfViewers] = useState(0)
+function Checkbox() {
+  const [checked, setChecked] = useState(true)
   return (
     <>
-    <p>The best show right now is {show}. It has {numberOfViewers} viewers</p>
-    <button onClick={()=> setShow("Squid Games")}>update show to squid games</button>
-    <button onClick={()=> setNumberOfViewers(numberOfViewers + 1)}>Increase Viewers</button>
-    <p>useState status is {status}</p>
-    <button onClick={()=> setStatus("Closed")}>Click me</button>
-    <button onClick={()=> setStatus("Open")}>Click me</button>
+    <input type="checkbox" value={checked} onChange={()=> setChecked(!checked)} />
     </>
   )
 }
 
 ReactDOM.render(
-  <App />,
+  <Checkbox />,
   document.getElementById('root')
 );
